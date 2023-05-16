bc407d34976f0c8f92e3375f187109028d3bd449
AngerOverApathy <violasian@gmail.com>
2023-05-15 20:38:41 -0700

    error handling added

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,5 +1,37 @@
+const { constants } = require('../constants')
+
 const errorHandler = (err, reg, res, next) => {
-    const statusCode = res.statusCode ? res.statusCode : 500
+    const statusCode = res.statusCode ? res.statusCode : 500;
+    switch (statusCode) {
+        case constants.VALIDATION_ERROR:
+            res.json({ 
+                title: 'Validation Failed', 
+                message: err.message, 
+                stackTrace: err.stack 
+            })
+            break;
+        case constants.UNAUTHORIZED:
+            res.json({ 
+                title: 'Validation Failed', 
+                message: err.message, 
+                stackTrace: err.stack 
+            })
+            break;
+        case constants.FORBIDDEN:
+            res.json({ 
+                title: 'Validation Failed', 
+                message: err.message, 
+                stackTrace: err.stack 
+            })
+            break;
+        case constants.NOT_FOUND:
+            res.json({ 
+                title: 'Not Found', 
+                message: err.message, 
+                stackTrace: err.stack })
+        default:
+            break;
+    }
 }
 
-module.exports = errorHandler
+module.exports = errorHandler;
