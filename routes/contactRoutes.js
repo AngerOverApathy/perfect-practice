eb31f6abd542694965b578f86aad6b6e47611e47
AngerOverApathy <violasian@gmail.com>
2023-05-15 19:35:22 -0700

    routes created

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,19 +2,23 @@ const express = require('express')
 const router = express.Router()
 
 router.route('/').get((req,res) =>{
-    res.status(200).json({ message:'Getting all contacts' })
+    res.status(200).json({ message:'Get contact information' })
 })
 
 router.route('/').post((req,res) =>{
     res.status(200).json({ message:'Create contacts' })
 })
-
-router.route('/').get((req,res) =>{
-    res.status(200).json({ message:'Getting all contacts' })
+router.route('/:id').get((req,res) =>{
+    res.status(200).json({ message:`Get contact information for ${req.params.id}` })
 })
 
-router.route('/').get((req,res) =>{
-    res.status(200).json({ message:'Getting all contacts' })
+router.route('/:id').put((req,res) =>{
+    res.status(200).json({ message:`Update contact information for ${req.params.id}` })
+})
+
+
+router.route('/:id').delete((req,res) =>{
+    res.status(200).json({ message:`Delete contact information for ${req.params.id}` })
 })
 
 module.exports = router;
