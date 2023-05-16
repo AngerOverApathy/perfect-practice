e8ae854520dfc0830141ce24d04a4bde732e2b83
AngerOverApathy <violasian@gmail.com>
2023-05-15 20:10:04 -0700

    route controller routes created

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,15 +1,32 @@
-//@desc Get all contacts
-//@route GET /api/contacts
-//@acces to API public
-const getContact = (req,res) =>{
-    res.status(200).json({ message:'Get contact information' })
+//@desc Get all contacts @route GET /api/contacts @acces to API public
+const getContacts = (req,res) =>{
+    res.status(200).json({ message:'Get all contact information' })
 }
 
-//@desc Create new contacts
-//@route POST /api/contacts
-//@acces to API public
+//@desc Get specific contacts @route GET /api/contacts/:id /@acces to API public
+const getOneContact = (req,res) =>{
+    res.status(200).json({ message:`Get contact information for ${req.params.id}` })
+}
+
+//@desc Create new contacts @route POST /api/contacts @acces to API public
 const createContact = (req,res) =>{
     res.status(201).json({ message:'Create contact information' }) //201 means resource created
 }
 
-module.exports = { getContact, createContact }
+//@desc Update new contacts @route PUT /api/contacts/:id @acces to API public
+const updateContact = (req,res) =>{
+    res.status(200).json({ message:`Update contact information for ${req.params.id}` })
+}
+
+//@desc delete new contacts @route POST /api/contacts/:id @acces to API public
+const deleteContact = (req,res) =>{
+    res.status(201).json({ message:'Delete contact information' }) //201 means resource created
+}
+
+module.exports = { 
+    getContacts, 
+    getOneContact, 
+    createContact, 
+    updateContact, 
+    deleteContact 
+}
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,22 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const { getContact, createContact } = require('../controllers/contactController')
+const { getContacts, getOneContact, createContact, updateContact, deleteContact } = require('../controllers/contactController')
 
-router.route('/').get(getContact)
-
-router.route('/').post(createContact)
-
-router.route('/:id').get((req,res) =>{
-    res.status(200).json({ message:`Get contact information for ${req.params.id}` })
-})
-
-router.route('/:id').put((req,res) =>{
-    res.status(200).json({ message:`Update contact information for ${req.params.id}` })
-})
+router.route('/').get(getContacts).post(createContact)
+router.route('/:id').get(getOneContact).put(updateContact).delete(deleteContact)
 
 
-router.route('/:id').delete((req,res) =>{
-    res.status(200).json({ message:`Delete contact information for ${req.params.id}` })
-})
+
+
 
 module.exports = router;
