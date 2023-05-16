b88505afd5779ad0838a41e682225282cf89fc4b
AngerOverApathy <violasian@gmail.com>
2023-05-15 19:20:19 -0700

    started routes

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000 //pull PORT from dotenv
+
+app.use('/api/contacts')
+
+app.listen(port, () =>{
+    console.log(`server running on ${port}`)
+})
